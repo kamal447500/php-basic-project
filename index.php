2c1099776965880cb1427e9988e81c985b94ab85
kamal447500 <52102324+kamal447500@users.noreply.github.com>
2019-12-17 22:56:45 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -55,7 +55,6 @@ if(isset($_GET['logout'])){
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
     	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    	<p> Not yet a Employee? <a href="employee.php">Add Employee</a> </p>
     <?php endif ?>
 </div>	
 	
@@ -77,21 +76,33 @@ if(isset($_GET['logout'])){
 			<td><?php echo $row['name']; ?></td>
 			<td><?php echo $row['salary']; ?></td>
 			<td>
-				<a href="employee.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
-				<div class="input-group">
-					<?php if ($update == true): ?>
-						<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
-					<?php else: ?>
-						<button class="btn" type="submit" name="save" >Save</button>
-					<?php endif ?>
-			
-				</div>
+				<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
 			</td>
 			<td>
-				<a href="employee.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+				<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
 			</td>
 		</tr>
 	<?php } ?>
 </table>
+	<form method="post" action="server.php" >
+		<div class="input-group">
+			<input type="hidden" name="id" value="<?php echo $id; ?>">
+		</div>
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="<?php echo $name; ?>">
+		</div>
+		<div class="input-group">
+			<label>Salary</label>
+			<input type="number" name="salary" value="<?php echo $salary; ?>">
+		</div>
+		<div class="input-group">
+			<?php if ($update == true): ?>
+				<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
+			<?php else: ?>
+				<button class="btn" type="submit" name="save" >Save</button>
+			<?php endif ?>
+		</div>
+	</form>
 </body>
 </html>
